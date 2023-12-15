17014202a319ef1750ebfb2c6e8d8e7e973fbf1e
j178 <10510431+j178@users.noreply.github.com>
2023-12-15 22:46:35 +0800

    use current time as last created at

--- watch/watch.go
+++ watch/watch.go
@@ -77,12 +77,9 @@ func Watch() error {
 		}
 	}
 
-	if len(issues) > 0 {
-		lastCreatedAt = issues[0].GetCreatedAt().Time
-		err = updateLastCreatedAt(ctx, owner, repo, lastCreatedAt)
-		if err != nil {
-			return err
-		}
+	err = updateLastCreatedAt(ctx, owner, repo, time.Now())
+	if err != nil {
+		return err
 	}
 	return nil
 }
