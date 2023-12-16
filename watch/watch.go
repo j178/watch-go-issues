0bc35ca7190c91e8198d71490ea17f19a99dceb6
j178 <10510431+j178@users.noreply.github.com>
2023-12-16 14:34:27 +0800

    fix timeout

--- watch/watch.go
+++ watch/watch.go
@@ -55,7 +55,7 @@ func Watch() error {
 		return err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 50000*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	src := oauth2.StaticTokenSource(
