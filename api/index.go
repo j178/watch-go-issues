bc516fd225e07fe7d251dd2d44d39df2db823f1c
j178 <10510431+j178@users.noreply.github.com>
2024-01-02 14:33:21 +0800

    add token auth

--- api/index.go
+++ api/index.go
@@ -2,11 +2,17 @@ package api
 
 import (
 	"net/http"
+	"os"
 
 	watchgoissues "github.com/j178/watch-go-issues/watch"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	secret := r.Header.Get("Authorization")
+	if secret != os.Getenv("SECRET") {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
 	err := watchgoissues.Watch()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
